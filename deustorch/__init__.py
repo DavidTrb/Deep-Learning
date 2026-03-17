f1878a91389187bfccfb3f3be3e2c40753b4819a
DavidTrb <161586150+DavidTrb@users.noreply.github.com>
2026-03-17 12:20:34 +0100

    Add files via upload

---
+++ deustorch/__init__.py
@@ -0,0 +1,5 @@
+import sys , os
+sys.path.insert (0,os.path.dirname(os.path.abspath("C:\Users\david.trueba\OneDrive - Universidad de Deusto\Escritorio\Proyecto DL\deustorch")))
+import deustorch .nn
+import deustorch .models
+import deustorch .optim
---
+++ deustorch/nn/__init__.py
@@ -0,0 +1,4 @@
+from . activations import Sigmoid , Tanh , ReLU
+from . loss import MSELoss , CrossEntropyLoss
+from . linear import Linear
+from . batchnorm import BatchNorm1d
